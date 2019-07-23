856a441345c8610665f4f6484b09faa179f9ccb4
Ivan Nieves Ardila <gykour.friki.gamer@gmail.com>
2019-07-23 15:27:20 -0500

    fetch table

--- Example3/Static/js/table.js
+++ Example3/Static/js/table.js
@@ -1,1 +1,19 @@
-alert("Funcionando")
+var print = document.getElementById("print")
+
+function show(){
+    fetch('tabla.json')
+    .then(data => data.json())
+    .then(data => {
+        // console.log(data.length)
+        for (let i = 0; i<data.length;i++){
+            print.innerHTML +=`
+                <tr>
+                <td>${data[i].id}</td>
+                <td>${data[i].nombre}</td>
+                <td>${data[i].email}</td>
+                <td>${data[i].estado ? "Activo" : "Eliminado"}</td>
+                </tr>
+            `
+        }
+    })
+}
