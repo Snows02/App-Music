2529cc7663ca4f88959dc2f2e77d71f7c3452c7e
Ivan Nieves Ardila <gykour.friki.gamer@gmail.com>
2019-07-23 14:53:12 -0500

    organizing directories

--- Example2/Static/js/fetch.js
+++ Example2/Static/js/fetch.js
@@ -19,7 +19,7 @@ function show(){
         console.log(data.results[0])
         contenido.innerHTML = `
         <img src="${data.results[0].picture.large}" width="100px" class="imgcircle">
-        <p><b>Nombre:</b> ${data.results[0].name.first} ${data.results[0].name.last} </p>
+        <p class="mayus"><b>Nombre:</b> ${data.results[0].name.first} ${data.results[0].name.last} </p>
         `
     })
     
