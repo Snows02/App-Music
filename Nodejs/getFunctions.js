caf588903b68eb4f4de05b80a34af439b09dec8b
Snows02 <nieves.ardila02@gmail.com>
2020-02-14 20:52:06 -0500

    Organizando el repo para iniciar el curso de js de platzi

--- Nodejs/getFunctions.js
+++
@@ -1,6 +0,0 @@
-// IMPORTANDO FUNCIONES PROPIAS -------------------------------------
-const math = require('./funciones.js');
-console.log(math);
-console.log(math.hello("Ivan"));
-console.log("La respuesta es: "+math.multiply(5,5));
-//-------------------------------------------------------------------
